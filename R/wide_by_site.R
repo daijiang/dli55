36831e82ff9bd9c1ae1533eebfa27da31aceacad
daijiang <daijianglee@gmail.com>
2014-04-10 17:52:52 -0500

    add wide_by_site functions

---
+++ R/wide_by_site.R
@@ -0,0 +1,23 @@
+#' Change long table into wide table for each site
+#'
+#' Produce wide quad by species data frame for each site from long table in a list. 
+#' Each element of the list is a quadrat by species data frame.
+#' 
+#' \code{wide_by_site} to change from long table to wide table for each site
+#' 
+#' @author Daijiang Li
+#' @import reshape2 plyr
+#' @param df data frame of vegtation data. Should have site, quad, sp columns.
+#' @return list. 
+
+# split sites into list, each element is a site by species data frame for one site
+wide_by_site=function(df){ 
+  aa=dlply(df, .(site), function(x) dcast(x, quad~sp,value.var="count",
+                                          fun.aggregate = length)) 
+  aa=llply(aa, function(x) {
+    rownames(x)=x$quad
+    x$quad=NULL
+    return(x)
+    }) 
+  aa
+}
---
+++ R/wide_by_site_rarefy.R
@@ -0,0 +1,21 @@
+# Resample survey data
+#' \code{resample} to resample quadrats from survey data
+#' 
+#' @author Daijiang Li
+#' 
+#' @param df data frame of vegtation data. Should have site, quad, sp columns. 
+#' It will call wide_by_site function inside.
+#' @param n number of ramdom sampling from quadrats of 2000s data for each site.
+#' @param q how many quadrats to sample out for 2000s data for all sites.
+
+
+wide_by_site_rarefy=function(df, n=1000, q = 20){ 
+  df1=wide_by_site(df)
+  aa=llply(df1, function (x) {
+    rlply(.n = n,  x[sample(dim(x)[1], q),])
+    }) 
+  for (i in 1: length(aa)){
+    names(aa[[i]])=rep(names(aa)[i], length(aa[[i]]))
+  }
+  aa
+}
